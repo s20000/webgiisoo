8fa7c46ab039d9de6c5479900bf7e53b3b8e452a
joe <joe@localhost>
2015-09-15 06:55:08 +0800

    bug fixes

--- src/main/java/framework/com/giisoo/framework/common/AccessLog.java
+++ src/main/java/framework/com/giisoo/framework/common/AccessLog.java
@@ -15,48 +15,46 @@ import com.giisoo.core.bean.UID;
  * @author joe
  * 
  */
-@DBMapping(table = "access_log")
+@DBMapping(collection = "access_log")
 public class AccessLog extends Bean {
 
-	/**
+    /**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	String id;
-	String ip;
-	String method;
-	String agent;
-	String url;
-	String sid;
-	String header;
-	String query;
-	long created;
-	long cost;
-	String handler;
+    String id;
+    String ip;
+    String method;
+    String agent;
+    String url;
+    String sid;
+    String header;
+    String query;
+    long created;
+    long cost;
+    String handler;
 
-	/**
-	 * Creates the AccessLog.
-	 * 
-	 * @param ip
-	 *            the ip
-	 * @param url
-	 *            the url
-	 * @param v
-	 *            the v
-	 */
-	public static void create(String ip, String url, V v) {
-		try {
-			long created = System.currentTimeMillis();
-			String id = UID.id(ip, url, created);
+    /**
+     * Creates the AccessLog.
+     * 
+     * @param ip
+     *            the ip
+     * @param url
+     *            the url
+     * @param v
+     *            the v
+     */
+    public static void create(String ip, String url, V v) {
+        try {
+            long created = System.currentTimeMillis();
+            String id = UID.id(ip, url, created);
 
-			Bean.insert(
-					v.set("id", id).set("ip", ip).set("url", url)
-							.set("created", created), AccessLog.class);
-		} catch (Exception e) {
-			// log.error(url, e);
-			// ignore
-		}
+            Bean.insert(v.set("id", id).set("ip", ip).set("url", url).set("created", created), AccessLog.class);
+        } catch (Exception e) {
+            // log.error(url, e);
+            // ignore
+        }
 
-	}
+    }
 }
