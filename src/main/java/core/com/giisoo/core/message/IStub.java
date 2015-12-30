e150f10525304d90861247d180e9b79d7f8f92a8
s20000 <yjiang@giisoo.com>
2015-12-30 12:30:24 +0800

    bug fixes

--- src/main/java/core/com/giisoo/core/message/IStub.java
+++
@@ -1,9 +0,0 @@
-package com.giisoo.core.message;
-
-import net.sf.json.JSONObject;
-
-public interface IStub {
-    public void onRequest(String to, String from, String src, byte flag, JSONObject header, JSONObject msg, byte[] attachment);
-
-    public void onResponse(String to, String from, String src, byte flag, JSONObject header, JSONObject msg, byte[] attachment);
-}
--- tools/js.min.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-java -jar yuicompressor-2.4.6.jar --preserve-semi --charset utf8 -o ../temp/t.min.js ../temp/t.js
-
-
